152dd90e36f8aa6f31a02099dad1f30fb0d656a7
Beatrizvn <beatriz-vn@hotmail.com>
2023-02-01 17:18:15 -0300

    detetive feito e aprovado pelo moj

--- Trabalho/Gomercindo.c
+++ Trabalho/Gomercindo.c
@@ -1,54 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-typedef struct Item
+typedef struct Pista
 {
     int id;
     int value;
     int next;
-} Item;
+} Pista;
 
 typedef struct Pilha
 {
-    Item *v;
+    Pista *p;
     int size;
     int elem;
 } Pilha;
 
-void criaPilha(Pilha *pilha, int s)
-{
-    pilha->v = malloc(sizeof(Pilha) * s);
+
+void criaPilha(Pilha *pilha, int s){
+    pilha->p = malloc(sizeof(Pilha) * s);
     pilha->size = s;
     pilha->elem = -1;
 }
 
-int estaVazia(Pilha *pilha)
-{
+int estaVazia(Pilha *pilha){
     return pilha->elem == -1;
 }
 
-int estaCheia(Pilha *pilha)
-{
+int estaCheia(Pilha *pilha){
     return pilha->elem == pilha->size;
 }
 
-Item espia(Pilha *pilha)
-{
-    return pilha->v[pilha->elem];
+Pista espia(Pilha *pilha){
+    return pilha->p[pilha->elem];
 }
 
-int empilha(Pilha *pilha, Item e)
-{
+int empilha(Pilha *pilha, Pista e){
     if (estaCheia(pilha))
         return 1;
 
     pilha->elem++;
-    pilha->v[pilha->elem] = e;
+    pilha->p[pilha->elem] = e;
     return 0;
 }
 
-int desempilha(Pilha *pilha)
-{
+int desempilha(Pilha *pilha){
     if (estaVazia(pilha))
         return 1;
 
@@ -56,61 +51,62 @@ int desempilha(Pilha *pilha)
     return 0;
 }
 
-void desalocaPilha(Pilha *pilha)
-{
+void desalocaPilha(Pilha *pilha){
     free(pilha);
 }
 
-void solve(Pilha *pilha, int n)
-{
-    Item e;
+int main(){
 
-    // Obter valores
-    while (n-- >= 1)
-    {
-        scanf("%d %d %d", &e.id, &e.value, &e.next);
-        empilha(pilha, e);
+    int numero_linhas;
+    scanf("%d", &numero_linhas);
+
+    struct Pista* pistas = (struct Pista*)malloc(sizeof(struct Pista) * numero_linhas);
+
+    int value,next,id;
+    for(int i = 0; i < numero_linhas; i++) {
+        scanf("%d %d %d", &id, &value, &next);
+        pistas[i].id = id;
+        pistas[i].value = value;
+        pistas[i].next = next;
     }
-
-    // fazer buscas completar o alg;
-    // ideia : trocar os valores ordenando linearmente
-
-    // obter fim
-    int a;
-    for (a = 0; a < pilha->elem; a++)
-    {
-        if (pilha->v[a].next == -1)
-        {
-            Item tt = pilha->v[a];
-            pilha->v[a] = pilha->v[pilha->elem];
-            pilha->v[pilha->elem] = tt;
-        }
-    }
-
-    int b, c;
-    for (b = 0; b < pilha->elem - 1; b++)
-    {
-        for (c = b + 1; c < pilha->elem - 1; c++)
-        {
-            if (pilha->v[b].next == pilha->v[c].id)
-            {
-                Item temp = pilha->v[c];
-                pilha->v[c] = pilha->v[b + 1];
-                pilha->v[b + 1] = temp;
-            }
-        }
-    }
-    int k;
-    for (k = 0; k < pilha->elem + 1; k++)
-        printf("%d\n", pilha->v[k].value);
-}
-
-int main()
-{
+    
     Pilha pilha;
-    int n;
-    scanf("%d", &n);
-    criaPilha(&pilha, n);
-    solve(&pilha, n);
+    criaPilha(&pilha,numero_linhas);
+    int fim_local;
+
+    // obter fim, o primeiro valor a ser empilhado, o que tem next == -1
+    for (int n = 0; n < numero_linhas; n++){
+        int fim = 0;
+        if (pistas[n].next == -1){
+            fim = 1;
+            fim_local = n;
+        }
+
+        if(fim == 1){
+            empilha(&pilha, pistas[n]);
+        }
+    }
+
+    //colocando os outros e passando pela lista varias vezes
+    for (int j = 0; j < numero_linhas; j++){
+        for (int i = 0; i < numero_linhas; i++){
+            //so empilha a psita se o valor next dela for o mesmo do id do topo/ultimo valor empilhado 
+            if (pistas[i].next == pistas[fim_local].id){
+                empilha(&pilha, pistas[i]);
+                fim_local = i;
+            }       
+        }
+    }
+    
+    //print
+    int k;
+    for (k = pilha.elem; k >= 0; k--)
+        printf("%d\n", pilha.p[k].value);
+/*
+3
+10 35 80
+20 50 -1
+80 57 20
+*/
     return 0;
 }
