8aa3d331ec8540bb6355a2ae312d654c49686c66
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2023-02-05 17:30:09 -0300

    +12 pontos

--- Trabalho/Ada.c
+++ Trabalho/Ada.c
@@ -1,135 +1,165 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <stdio.h>
-#include <stdlib.h>
 typedef int Item;
+
 typedef struct Node
 {
-    Item c;
+    Item item;
     struct Node *next;
-    struct Node *prev;
+    struct Node *previous;
 } Node;
 
 typedef struct Head
 {
+    Node *first;
+    Node *last;
     int size;
-    struct Node *begin;
-    struct Node *end;
 } Head;
 
-int createHead(Head *head)
+int createHead(Head *x)
 {
-    head->begin = NULL;
-    head->end = NULL;
-    head->size = 0;
-    return 0;
+    x->first = NULL;
+    x->last = NULL;
+    x->size = 0;
+    return 1;
 }
 
-int pushFrist(Head *head, Item e)
+int estaVazia(Head *x)
 {
-    Node *node = malloc(sizeof(Node));
-    if (node == NULL)
-        return 1;
-
-    node->c = e;
-    node->next = NULL;
-    node->prev = NULL;
-
-    head->size++;
-    head->begin = node;
-    head->end = node;
-
-    return 0;
+    return x->first == NULL;
 }
 
-int pushToBack(Head *head, Item e)
+int pushFrist(Head *x, Item i)
 {
-    Node *node = malloc(sizeof(Node));
-    if (node == NULL)
-        return -1;
+    Node *novoNo = malloc(sizeof(Node));
 
-    node->c = e;
-    node->prev = head->end;
-    node->next = NULL;
+    if (novoNo == NULL)
+        return 0;
+    novoNo->previous = NULL;
+    novoNo->item = i;
 
-    head->end->next = node;
-
-    head->size++;
-    head->end = node;
-    return 0;
-}
-
-int pushLink(Head *head, Item e)
-{
-    if (head->size == 0)
+    if (x->size == 0)
     {
-        return pushFrist(&head, e);
+        novoNo->next = NULL;
+        x->first = novoNo;
+        x->last = novoNo;
     }
-    return pushToBack(&head, e);
+    else
+    {
+        x->first->previous = novoNo;
+        novoNo->next = x->first;
+        x->first = novoNo;
+    }
+
+    x->size++;
+    return 1;
 }
 
-int popFrist(Head *head)
+int popFrist(Head *x)
 {
-    if (head->size == 0)
+    // se a lista estiver vazia,ele não faz nada
+    if (x->size == 0)
+    {
+        printf("No job for Ada?\n");
+        return 0;
+    }
+
+    Node *t = x->first;
+
+    printf("%d\n", x->first->item);
+    x->first = x->first->next;
+    if (x->first == NULL)
+        x->last = NULL;
+
+    x->size--;
+    free(t);
+    return 1;
+}
+
+int pushToBack(Head *x, Item i)
+{
+
+    Node *novoNo = malloc(sizeof(Node));
+    if (novoNo == NULL)
+        return 0;
+    novoNo->next = NULL;
+    novoNo->item = i;
+
+    if (x->size == 0)
+    {
+        x->first = novoNo;
+        x->last = novoNo;
+        novoNo->previous = NULL;
+    }
+    else
+    {
+        x->last->next = novoNo;
+        novoNo->previous = x->last;
+        x->last = novoNo;
+    }
+
+    x->size++;
+    return 1;
+}
+
+int popLast(Head *x)
+{
+    // se a lista estiver vazia,ele não faz nada
+    if (x->size == 0)
     {
         printf("No job for Ada?\n");
         return 1;
     }
-    Node *newBegin = head->begin->next;
-    printf("%d\n", head->begin->c);
-    free(head->begin);
-    newBegin->prev = NULL;
-    head->begin = newBegin;
-    head->size--;
-    return 0;
+    Node *t = x->last;
+
+    printf("%d\n", x->last->item);
+
+    x->last = x->last->previous;
+    if (x->last != NULL)
+        x->last->next = NULL;
+    else
+        x->first = NULL;
+
+    x->size--;
+    free(t);
 }
-
-int popLast(Head *head)
-{
-    if (head->size == 0)
-    {
-        printf("No job for Ada?\n");
-        return 1;
-    }
-    Node *newLast = head->end->prev;
-
-    printf("%d\n", head->end->c);
-
-    free(head->end->prev);
-    head->end = newLast;
-    head->size--;
-    return 0;
-}
-
-int reverse(Head *head) {}
 
 int main()
 {
-    int q, i, n, flag = 0;
+    // Utiliza-se de uma flag para definir
+    // se queremos ordem reversa ou não
+    int q, i, n, reverse = 0;
     char com[1001];
     Head head;
     createHead(&head);
 
+    // extrai informação
     scanf("%d", &q);
 
+    // para com[0]
+    // se for b, ele printa o ultimo e o exclui
+    // se for f, ele printa o primeiro e o exclui
+    // se for p, ele puxa para trás da lista
+    // se for t ele puxa para frente da lista
+    // se for r ele troca o sinal da flag na qual inverte ou não
+    // as operações
     for (i = 0; i < q; i++)
     {
         scanf("%s", com);
         if (com[0] == 'b')
-            if (flag == 0)
+            if (reverse == 0)
                 popLast(&head);
             else
                 popFrist(&head);
         if (com[0] == 'f')
-            if (flag == 0)
+            if (reverse == 0)
                 popFrist(&head);
             else
                 popLast(&head);
         if (com[0] == 'p')
         {
             scanf("%d", &n);
-            if (flag == 0)
+            if (reverse == 0)
                 pushToBack(&head, n);
             else
                 pushFrist(&head, n);
@@ -137,17 +167,23 @@ int main()
         if (com[0] == 't')
         {
             scanf("%d", &n);
-            if (flag == 0)
+            if (reverse == 0)
                 pushFrist(&head, n);
             else
                 pushToBack(&head, n);
         }
         if (com[0] == 'r')
         {
-            if (flag == 0)
-                flag = 1;
-            if (flag == 1)
-                flag = 0;
+            if (reverse == 0)
+            {
+                reverse = 1;
+                continue;
+            }
+            if (reverse == 1)
+            {
+                reverse = 0;
+                continue;
+            }
         }
     }
 
---
+++ Trabalho/Torre.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int alt, larg;
+    scanf("%d %d", &alt, &larg);
+    char camp[alt][larg];
+    return 0;
+}
